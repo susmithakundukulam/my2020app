94ccf5885d28216c968124ae361ee06efedd643f
Susmitha <susmithakundukulam@gmail.com>
2021-01-11 15:46:06 +0530

    @PostConstruct used to get list for testing

--- src/main/java/com/my2020app/controllerImpl/EmployeeRestControllerImpl.java
+++ src/main/java/com/my2020app/controllerImpl/EmployeeRestControllerImpl.java
@@ -3,6 +3,7 @@ package com.my2020app.controllerImpl;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 
 import com.my2020app.controller.EmployeeRestController;
@@ -11,15 +12,25 @@ import com.my2020app.model.Employee;
 
 @Controller
 public class EmployeeRestControllerImpl implements EmployeeRestController{
+	
+	public List<Employee> employees = new ArrayList<Employee>();
+
+	@PostConstruct
+	public List<Employee> employees(){
+		employees.add(new Employee("Sus", "Tom"));
+		employees.add(new Employee("Tom", "Anto"));
+		employees.add(new Employee("Chris", "Tom"));
+		return employees;
+	}
 
 	@Override
 	public ResponseMessage getEmployeeList() {
 		System.out.println("..........getEmployeeList..........");
 		ResponseMessage response=new ResponseMessage();
-		List<Employee> employees = new ArrayList<Employee>();
-		employees.add(new Employee("Sus", "Tom"));
-		employees.add(new Employee("Tom", "Anto"));
-		employees.add(new Employee("Chris", "Tom"));
+//		List<Employee> employees = new ArrayList<Employee>();
+//		employees.add(new Employee("Sus", "Tom"));
+//		employees.add(new Employee("Tom", "Anto"));
+//		employees.add(new Employee("Chris", "Tom"));
 
 		response.addPayload("employeeList", employees);
 		return response;
